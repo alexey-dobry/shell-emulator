fe5b473f4dc6de6f70a0524a88cd1c580a5f1124
Alexey  Nehaenko <Nehaenkog23@gmail.com>
2024-10-09 18:49:45 +0300

    Исправления в cd и ls; добавлен файл для тестирования; добавлен zip файл виртуальной файловой системы

---
+++ emulator.py
@@ -0,0 +1,103 @@
+import sys
+from zipfile import ZipFile
+import argparse
+class ShellEmulator:
+    def __init__(self,user_name ="default",path_to_arxiv="./file_system.zip",path_to_script = ""):
+        self.user_name = user_name
+        self.path_to_arxiv = path_to_arxiv
+        self.path_to_script = path_to_script
+        self.virtual_sys_folder = ZipFile(path_to_arxiv, 'a')
+        self.cwd = '/'
+    def run_script(self,script_path):
+        with open(script_path,'r') as script:
+            for line in script:
+                self.execute_command(line)
+    def execute_command(self,command):
+        if not command:
+            return
+        
+        commands = command.split()
+        cmd = commands[0]
+        args = commands[1:]
+
+        if cmd == 'cd':
+            self.cd(args)
+        elif cmd == 'ls':
+            self.ls(args)
+        elif cmd == 'exit':
+            self.exit()
+        elif cmd == 'uname':
+            self.uname(args)
+        elif cmd == 'mkdir':
+            self.mkdir(args)
+        elif cmd == 'pwd':
+            self.pwd()
+        else:
+            print(f"{cmd}: unknowd command")
+    
+    def cd(self,args):
+        if not args:
+            return
+        target = args[0]
+        list_of_paths = self.virtual_sys_folder.namelist()
+        if target == '/':
+            self.cwd = '/'
+        elif target == '..':
+            if self.cwd == '/':
+                return
+            else:
+                self.cwd = '/'.join(self.cwd.split('/')[:-2]) +'/'
+        elif target == '.':
+            return
+        elif self.cwd[1:] + target + '/' in list_of_paths: #Обработка случая абсолютного пути
+            self.cwd += target + '/'
+        elif target[1:] + '/' in list_of_paths: #Обработка случая относительного пути
+            self.cwd = target + '/'
+        else:
+            print(f"No such file or directory")
+            return
+
+    def ls(self,args):
+        list_of_paths = self.virtual_sys_folder.namelist()
+        for path in list_of_paths:
+            if( self.cwd == '/' + path[0:len(self.cwd) - 1] and self.cwd.count('/') == path.count('/')):
+                print(path[len(self.cwd) - 1: path.rfind('/')])
+    def exit(self):
+        print("Bye!")
+        self.virtual_sys_folder.close()
+        exit(0)
+    def uname(self,args):
+        if not args:
+            print("Linux")
+        elif args[0] == "-a":
+            print(f"Linux {self.user_name} 5.15.0-1-generic x86_64 GNU/Linux")
+    def mkdir(self,args):
+        folder_name = args[0]
+        if self.cwd == '/':
+            path = folder_name + '/'
+        else:
+            path = self.cwd[1:] + folder_name + '/'
+        self.virtual_sys_folder.writestr(path, '')
+    def pwd(self):
+        print(self.cwd)
+    def run(self):
+        if(self.path_to_script != None and self.path_to_script[0:2] == "./"):
+            self.run_script(self.path_to_script)
+        try:
+            while True:
+                command = input(f"{self.user_name}@{self.cwd}> ")
+                self.execute_command(command)
+        except KeyboardInterrupt:
+            self.exit()
+
+def args_parser():
+    parser = argparse.ArgumentParser(description="Эмулятор Shell")
+    parser.add_argument("user_name",nargs="?",help="Имя пользователя",default="username")
+    parser.add_argument("path_to_arxiv",nargs="?",help="Путь до виртуальной файлово системы",default="./file_system.zip")
+    parser.add_argument("path_to_script",nargs="?",default=None)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args=args_parser()
+    emulator = ShellEmulator(args.user_name,args.path_to_arxiv,args.path_to_script)
+    emulator.run()
---
+++ tests.py
@@ -0,0 +1,78 @@
+import unittest
+import time
+from emulator import ShellEmulator
+
+class TestShellEmulator(unittest.TestCase):
+    def setUp(self):
+        """Подготовка перед каждым тестом"""
+        self.emulator = ShellEmulator(user_name="some_buddy",path_to_arxiv="./file_system.zip", path_to_script=None)
+
+    def test_ls_root(self):
+        """Тест команды ls в корневой директории"""
+        result = self._capture_output(self.emulator.ls, [])
+        self.assertIn('new_folder', result)  # Проверяем, что 'new_folder' присутствует в выводе
+
+    def test_cd_and_ls(self):
+        """Тест команды cd и ls внутри папки"""
+        self.emulator.cd(["new_folder"])  # Переходим в папку 'new_folder'
+        result = self._capture_output(self.emulator.ls, [])
+        self.assertIn('content', result)  # Проверяем, что 'content' находится в 'new_folder'
+
+    def test_cd_back_to_root(self):
+        """Тест команды cd для возвращения в корневую директорию"""
+        self.emulator.cd(["new_folder"])  # Переходим в папку 'new_folder'
+        self.emulator.cd([".."])  # Возвращаемся в корневую директорию
+        self.assertEqual(self.emulator.cwd, "/")  # Проверяем, что текущая директория - это корень
+    
+    def test_cd_absolute_path(self):
+        """Тест команды  cd для перемещения по абсолютному пути"""
+        self.emulator.cd(["/new_folder/content"])
+        self.assertEqual(self.emulator.cwd,"/new_folder/content/") # Проверка, что текущий путь это введенный путь
+
+    def test_uname(self):
+        """Тест команды uname"""
+        result = self._capture_output(self.emulator.uname, [])
+        self.assertEqual(result.strip(), "Linux")  # Проверяем, что команда uname возвращает 'Linux'
+
+    def test_uname_with_a(self):
+        """Тест команды uname с опцией -a"""
+        result = self._capture_output(self.emulator.uname, ["-a"])
+        self.assertIn("Linux some_buddy 5.15.0-1-generic x86_64 GNU/Linux\n", result)  # Проверяем вывод с опцией -a
+
+    def test_cd_nonexistent_directory(self):
+        """Тест ошибки при переходе в несуществующую директорию"""
+        result = self._capture_output(self.emulator.cd, ["none_dir"])
+        self.assertIn("No such file or directory", result)  # Проверяем, что выводится сообщение об ошибке
+    
+    def test_mkdir_in_root(self):
+        """Тест создания директории в корневой директории"""
+        self.emulator.mkdir(["2_directory"])
+        result = self._capture_output(self.emulator.ls, [])
+        self.assertIn('2_directory', result) # Проверяем, что в выводе ls появилась созданная директория
+    
+    def test_mkdir_in_folder(self):
+        """Тест создания директории в другой директории"""
+        self.emulator.cd(["new_folder"])
+        self.emulator.mkdir(["another_dir"])
+        result = self._capture_output(self.emulator.ls, [])
+        self.assertIn('another_dir',result) # Проверяем, что в выводе ls появилась созданная директория
+
+    def test_exit(self):
+        """Тест команды exit"""
+        with self.assertRaises(SystemExit):  # Ожидаем завершения программы
+            self.emulator.exit()
+
+    def _capture_output(self, func, args):
+        from io import StringIO
+        import sys
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+            func(args)
+            return out.getvalue()
+        finally:
+            sys.stdout = saved_stdout
+
+if __name__ == '__main__':
+    unittest.main()
